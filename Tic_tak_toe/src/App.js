76626d0f1d8d63664aa11f53f9c7eb6445c64c40
vishal <mgvishalctr@gmail.com>
2025-04-03 12:04:21 +0530

    push

---
+++ Tic_tak_toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TicTocToy } from './Component/TicTacToy/TicTocToy';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTocToy/>
+    </div>
+  );
+}
+
+export default App;
